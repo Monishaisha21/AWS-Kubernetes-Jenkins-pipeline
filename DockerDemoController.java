3880775eaf24d1bfeae968931cd6c57c6db50b22
Monishaisha21 <113742098+Monishaisha21@users.noreply.github.com>
2023-03-16 16:19:23 +0530

    Create DockerDemoController.java

---
+++ DockerDemoController.java
@@ -0,0 +1,13 @@
+package com.jhooq.Jhooqk8s.ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerDemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello - This is my message";
+    }
+}
